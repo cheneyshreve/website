8e2babc072626e2b6a29b3e134fbd00083a8ac2f
Tarka Wilcox <cheney.shreve@gmail.com>
2018-07-10 12:35:04 -0700

    added gifs, updated blog

--- src/components/Software.js
+++ src/components/Software.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import NavBar from './NavBar.js';
 import Footer from './Footer.js';
 import Jumbotron from './Jumbotron.js';
-
+import ImageOne from './../images/usaid-demo.gif';
+import ImageTwo from './../images/hdt-demo.gif';
 
 class Software extends Component {
   render() {
@@ -15,50 +16,27 @@ class Software extends Component {
              <div className="col-md-8 blog-main">
 
                <div className="blog-post">
-                 <h2 className="blog-post-title">Building a simple web tool with React & Rails</h2>
-                 <p className="blog-post-meta">June 2018 by Cheney</p>
+                 <h2 className="blog-post-title">Lessons learned: Loving that ugly monster app</h2>
+                 <p className="blog-post-meta">July 2018 by Cheney</p>
 
                  <h3>Motivation</h3>
                   <p>Nearing the end of my training in software development with Bloc, one of our projects is to build an app that will serve as a "proof-of-concept" and later grow into the basis for our own open source project. Coming into software development from working in environmental management and sustainable development, I often found colleagues were frustrated with the overcomplexity of different data warehouses. Phrases like "it has too many options to click through or too many buttons" or questions such as "why do I need a tutorial just to use your website?" were not uncommon. I started building a very simple web application that enables users to quickly query, graph and download datasets from popular development databases, no tutorials or training required. The idea being to eventually add additional databases and data visualization options.
                  </p>
                  <h3>Challenges</h3>
-
                  <p>Initially, I was planning to build the app with React only, since the data queries are sent to an external API to retrieve the data. My intent was never to recreate the development database(s), rather, to make it easier to search popular datasets. However, I ran into some challenges connecting to the development database, mainly, it was often inaccessible or pretty slow so I decided to make an "archive" database of my own to store a few sample indicators, say for recent years or popular countries,to allow the user to access some data in the event that the external development database was not responding. Rails-5 has a light weight API only option that is a great fit for my purposes.</p>
                 <br />
-                <p>The next bump in the road came with seeding the database. Prior projects that I had worked on in Rails had used filler data that was populated with the faker gem. The tables had turned, and I needed an easy way to seed my rails db using the indicator data. My first attempt was to read in the file as a json into seeds.db, which is an easy way to seed a rails db. However, the file size was too large. I ended up crashing my computer, oops, back to the drawing board. I knew there had to be a more efficient way to do this. After a quick skim of the Rails docs and StackOverflow, my second attempt was to query the external API from a model and try seeding the database that way. That was OK, but it was slow. That interim step got me on the right track to the most efficient route, which is making the external api call from seeds.rb using the json and open ssl libraries. This solution felt much better, it was a heck of a lot faster, and it made a lot more sense.</p>
 
-             <pre>{`
-require 'open-uri'
-require 'json'
+              <h3>Lessons learned: loving my little monster app </h3>
+              <p>What do you do when you encounter an API or other resource that is not as performant as you would like, but you still need or want to use that resource in your app?</p>
+              <p>In this case, I found a database that has really valuabale data on womens health and wanted to stick with it for that reason. It does not make for a shiny, loveable app, but it gives me a challenge to struggle with.</p>
+              <p>I can see others have struggled with it too. Take for example this gif of an example project someone made using this database. The jQuery code behind it looks great, but the API response is very slow.</p>
 
-InfantMortality.delete_all
-=begin
-read surveyYear file from DHS for top 10 countries with high infant mortality rates in database
-Afghanistan (AF), Mali (ML), Central African Republic (CF), Guinea (GN), Chad (TD), Niger (NI), Angola (AO),
-Burkina Faso (BF), Nigeria (NG), Sierra Leone (SL)
-=end
-open("https://api.dhsprogram.com/rest/dhs/data?countryIds=AF,ML,CF,TD,NI,AO,BF,NG,SL&indicatorIds=CM_ECMR_C_IMR") do |infantrates|
-  infantrates.read.each_line do |rate|
-    infant_data = JSON.parse(rate)
-     @infant_mortalities = infant_data["Data"]
-  end
-end
+                    <img src={ImageOne} />
 
-@infant_mortalities.each do |key, value|
-  data = []
-   object = {
-     DataId: key["DataId"],
-     Indicator: key["Indicator"],
-     SurveyId: key["SurveyId"],
-     IsPreferred: key["IsPreferred"],
-     Value: key["Value"],
-     SDRID: key["SDRID"],
-     ...
-   }
-   data << object
- InfantMortality.create!(data)
-end
-                `}</pre>
+              <p>In an attempt to make this more performant, I translated the code into React, see the second gif below. React makes it much faster, but it is still slow by todays standard. Plus, the UI needs a lot more love and care, an essential next step in my plan!</p>
+
+                 <img src={ImageTwo} />
+
 
                  <p>Another challenge I faced pertained to populating the dropdown menus in React. I am currently using the dropdown menus to let the user interactively build the query, e.g. first they select a country, which then triggers an external call to the API to grab the years available for that country, as these are different for different countries and indicators. Next the user selects the surey year, and then the available indicators are obtained, roughly 2500. I stored the various values (country, survey year, indicator) in state as arrays, then mapped over them inside the render function (with a few promises made in between to obtain the data). This seemed logical, however, I found that I was not able to select the first item from the menu, although it was automatically selected. To correct this, I ended up using a filler value as the first dropdown item, e.g. "please select a value".</p>
 
@@ -119,7 +97,7 @@ end
   ...
   `}</pre>
 
-  <p>A final challege I encountered of the frontend was that the data visualization library I had chosen, react-vis, has no export capabilities. I knew this at the outset, I had wanted to use highcharts but kept getting errors with the install that could not quickly be resolved, so I decided to give the other library a go. To address this challege, I used html2canvas, which allows you to easily store an image from a dom component with a few basic functions.</p>
+  <p>A final challege I encountered of the frontend was that the data visualization library I had chosen, react-vis, has no export capabilities. I knew this at the outset, I had wanted to use highcharts but kept getting errors with the install that could not quickly be resolved, so I decided to give the other library a go. To address this challege, I used html2canvas, which allows you to easily store an image from a dom component by writing some JavaScript functions.</p>
 
 
   <pre>{`
@@ -143,22 +121,6 @@ import html2canvas from 'html2canvas';
     a.download = 'graph.png';
     a.click();
   }
-
-  // function to enable user to save data as .csv file
-  saveCSV(){
-    var csvData = [ [this.state.selectedIndicator, this.state.selectedCountry, this.state.selectedYear, this.state.xvalues],["","","",this.state.yvalues]];
-    var dataString = '';
-    var csvContent = "data:text/csv;charset=utf-8,";
-    csvData.forEach(function(infoArray, index){
-       dataString = infoArray.join(",");
-       csvContent += index < infoArray.length ? dataString + "\n" : dataString;
-    });
-    var encodedUri = encodeURI(csvContent);
-    var link = document.createElement("a");
-    link.setAttribute("href", encodedUri);
-    link.setAttribute("download", .... csv);
-    link.click();
-   }
     `}
     </pre>
                  <h3>Learning outcomes</h3>
